9e659ef4026260d4b6b070bf3da4a93f4e8cb1c0
Beth C <147827249+beth-cl@users.noreply.github.com>
2024-10-08 19:55:46 +0100

    Create gprog2.cpp

---
+++ gprog2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <chrono>
+#include <thread>
+using namespace std;
+
+class Tile
+{
+
+};
+
+class Map : public Tile {
+    string Tile_Map[10][10];
+};
+
+class weapon {
+    public:
+        int weapon_durability, weapon_up_cost, weapon_damage;
+        string weapon_name;
+
+        void upgrade_weapon() {
+
+        }
+};
+
+class player : public weapon
+{
+    public:
+        int x_player, y_player, hp_player, gold_player;
+        int player_attack = 1;
+
+        int attack_monsters() {
+            return 0;
+         }
+
+
+};
+
+class gold {
+    public:
+        int rand_gold() {
+            return 0;
+        }
+
+};
+
+class monster : public Tile {
+    public:
+        int mon_x, mon_y, mon_hp, attack_val, mon_gold;
+
+        void mon_fight() {
+
+        }
+
+};
+
+int main() {
+    srand(time(NULL));
+    cout << "hello";
+}   
